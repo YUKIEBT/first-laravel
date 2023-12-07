92451404475815f9c812c68a271bc69392ee8b59
YUKIEBT <144157485+YUKIEBT@users.noreply.github.com>
2023-12-08 00:09:31 +0900

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\MiddlewareController;
+use App\Http\Controllers\SessionController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/', [TestController::class, 'index']);
+
+// Route::get("/test", [TestController::class, "index"]);
+
+Route::get("/", [TestController::class, "index"]);
+Route::post("/", [TestController::class, "post"]);
+Route::get('/middleware', [MiddlewareController::class, 'index']);
+Route::post('/middleware', [MiddlewareController::class, 'post'])->middleware('first');
+Route::get('/session', [SessionController::class, 'getSes']);
+Route::post('/session', [SessionController::class, 'postSes']);
